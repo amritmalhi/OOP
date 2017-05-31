02949a9a258ec6d6f95cbc1322f127bd4f7b4102
Amrit <amritsinghmalhi@gmail.com>
2017-05-31 16:41:43 +0200

    Practicum Week 1

--- template-week-1/main.cpp
+++ template-week-1/main.cpp
@@ -7,17 +7,27 @@
 #include "rectangle.hpp"
 #include "circle.hpp"
 
+#include "filled_rectangle.hpp"
+#include "my_object.hpp"
+
 int main(int argc, char **argv){
-   window w( 128, 64, 2 );
+   window w( 128, 64, 2);
    
-   line diagonal_line( w, 5, 5, 30, 40 );
-   diagonal_line.print();
    
-   rectangle box( w, 20, 10, 30, 20 );
-   box.print();
+   //line diagonal_line( w, 5, 5, 30, 40 );
+   //diagonal_line.print();
    
-   circle ball( w, 70, 30, 20 );
-   ball.print();
+   //rectangle box( w, 10, 5, 100, 60 );
+   //box.print();
+   
+   //circle ball( w, 70, 30, 20 );
+   //ball.print();
+   
+   //filled_rectangle doos(w, 50, 5, 100, 60);
+   //doos.draw();
+   
+   my_object mo(w, false);
+   mo.draw();
    
    return 0;
 }
